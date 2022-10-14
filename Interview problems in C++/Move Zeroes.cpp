ed3109f76b236896b857d44b0353058abe9df56d
dilippatnana <100085952+dilippatnana@users.noreply.github.com>
2022-10-14 21:36:58 +0530

    Update Move Zeroes.cpp

--- Interview problems in C++/Move Zeroes.cpp
+++ Interview problems in C++/Move Zeroes.cpp
@@ -1,3 +1,7 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
